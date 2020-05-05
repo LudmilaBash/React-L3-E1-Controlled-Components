b7e422bf154909523573a1b360a280f7ba384d5e
LudmilaBash <ludmiladvmnt@gmail.com>
2020-05-05 23:40:33 +0000

    Finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+   constructor(props){
+   super(props);
+   this.state={echo:""}; 
+   };
+   createEcho = (event)=>{
+    this.setState({echo:event.target.value})
+   }
+
+  render(){
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +20,10 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something"  
+           onChange={this.createEcho}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.echo}</p>
         </div>
       </div>
     );
